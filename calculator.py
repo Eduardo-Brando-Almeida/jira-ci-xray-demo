fd87556c8a14388c3ceb6f107b50d45750e446dd
Eduardo Brando Almeida <dugu2005@gmail.com>
2025-10-30 20:59:46 -0300

    First Commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def soma(a, b):
+    return a+b
+    
+def subtrai(a, b):
+    return a-b
+
+print(soma(10,20))
+print(subtrai(5,6))
---
+++ test_calculator.py
@@ -0,0 +1,7 @@
+from calculator import soma, subtrai
+
+def test_QASoma():
+    assert soma(2,3) == 5
+
+def test_QASoma():
+    assert soma(5,3) == 5
