6962fab60b3bc7fcec3e339fc6813fb9c9325565
Eduardo Brando Almeida <dugu2005@gmail.com>
2025-10-30 21:26:39 -0300

    Adiciona teste Q2E-6 e integração com Xray

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 from calculator import soma, subtrai
 
-def test_QASoma():
+def test_Q2E_6_soma():
     assert soma(2,3) == 5
 
-def test_QASoma():
-    assert soma(5,3) == 5
+def test_Q2E_8_soma():
+    assert subtrai(5,3) == 5
